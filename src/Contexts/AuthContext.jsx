c90614847e0dd645f2fcc6d379af546b8d968795
varunn15 <varunkumaryt54@gmail.com>
2026-07-21 18:03:31 +0530

    fix: add auth context

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -12,6 +12,8 @@ export const useAuth = () => {
   return context;
 };
 
+
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
